0884e3f466bb7ee9f0db9ad9ae445f2c5345fe40
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2021-10-03 20:55:36 +0200

    Create Ch1.py

---
+++ Ch1.py
@@ -0,0 +1,12 @@
+# Read test and sample submission data
+test = pd.read_csv('test.csv')
+sample_submission = pd.read_csv('sample_submission.csv')
+
+# Show the head() of the sample_submission
+print(sample_submission.head())
+
+# Get predictions for the test set
+test['sales'] = rf.predict(test[['store', 'item']])
+
+# Write test predictions using the sample_submission format
+test[['id', 'sales']].to_csv('kaggle_submission.csv', index=False)
