43f04241a5ac04f05d9ec8b6598f02d48f8ae43a
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2021-10-04 00:06:28 +0200

    Create Ch2.py

---
+++ Ch2.py
@@ -0,0 +1,44 @@
+'''
+    Solution Workflow:
+    1. Understand the problem (data type, problem type, evaluate matric)
+    2. EDA 
+    3. Local Validation 
+    4. Modeling
+    '''
+    
+import numpy as np
+
+# Import MSE from sklearn
+from sklearn.metrics import mean_squared_error
+
+# Define your own MSE function
+def own_mse(y_true, y_pred):
+  	# Raise differences to the power of 2
+    squares = np.power(y_true - y_pred, 2)
+    # Find mean over all observations
+    err = np.mean(squares)
+    return err
+
+print('Sklearn MSE: {:.5f}. '.format(mean_squared_error(y_regression_true, y_regression_pred)))
+print('Your MSE: {:.5f}. '.format(own_mse(y_regression_true, y_regression_pred)))
+
+import numpy as np
+
+# Import log_loss from sklearn
+from sklearn.metrics import log_loss
+
+# Define your own LogLoss function
+def own_logloss(y_true, prob_pred):
+  	# Find loss for each observation
+    terms = y_true * np.log(prob_pred) + (1 - y_true) * np.log(1 - prob_pred)
+    # Find mean over all observations
+    err = np.mean(terms) 
+    return -err
+
+print('Sklearn LogLoss: {:.5f}'.format(log_loss(y_classification_true, y_classification_pred)))
+print('Your LogLoss: {:.5f}'.format(own_logloss(y_classification_true, y_classification_pred)))
+
+''' 
+Initial EDA 
+
+'''
