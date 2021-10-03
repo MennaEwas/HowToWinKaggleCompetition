28c33219563c722c26a196c9af5e610a619ed660
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2021-10-04 00:03:39 +0200

    Update Ch1.py

--- Ch1.py
+++ Ch1.py
@@ -11,3 +11,57 @@ test['sales'] = rf.predict(test[['store', 'item']])
 # Write test predictions using the sample_submission format
 test[['id', 'sales']].to_csv('kaggle_submission.csv', index=False)
 
+import xgboost as xgb
+
+# Create DMatrix on train data
+dtrain = xgb.DMatrix(data=train[['store', 'item']],
+                     label=train['sales'])
+
+# Define xgboost parameters
+params = {'objective': 'reg:linear',
+          'max_depth': 2,
+          'silent': 1}
+
+# Train xgboost model
+xg_depth_2 = xgb.train(params=params, dtrain=dtrain)
+#change the max_depth 2, 8, 15
+from sklearn.metrics import mean_squared_error
+
+dtrain = xgb.DMatrix(data=train[['store', 'item']])
+dtest = xgb.DMatrix(data=test[['store', 'item']])
+
+# For each of 3 trained models
+for model in [xg_depth_2, xg_depth_8, xg_depth_15]:
+    # Make predictions
+    train_pred = model.predict(dtrain)     
+    test_pred = model.predict(dtest)          
+    
+    # Calculate metrics
+    mse_train = mean_squared_error(train['sales'], train_pred)                  
+    mse_test = mean_squared_error(test['sales'], test_pred)
+    print('MSE Train: {:.3f}. MSE Test: {:.3f}'.format(mse_train, mse_test))
+    
+    '''
+    Solution Workflow:
+    1. Understand the problem (data type, problem type, evaluate matric)
+    2. EDA 
+    3. Local Validation 
+    4. Modeling
+    '''
+    
+    mport numpy as np
+
+# Import MSE from sklearn
+from sklearn.metrics import mean_squared_error
+
+# Define your own MSE function
+def own_mse(y_true, y_pred):
+  	# Raise differences to the power of 2
+    squares = np.power(y_true - y_pred, 2)
+    # Find mean over all observations
+    err = np.mean(squares)
+    return err
+
+print('Sklearn MSE: {:.5f}. '.format(mean_squared_error(y_regression_true, y_regression_pred)))
+print('Your MSE: {:.5f}. '.format(own_mse(y_regression_true, y_regression_pred)))
+
